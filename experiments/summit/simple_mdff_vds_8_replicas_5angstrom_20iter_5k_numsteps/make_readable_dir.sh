e658f87faabdfb487ec4eae54b8277858e3b46c6
Hyungro Lee <hroe.lee@gmail.com>
2021-01-06 12:04:27 -0500

    fix

--- experiments/summit/simple_mdff_vds_8_replicas_5angstrom_20iter_5k_numsteps/make_readable_dir.sh
+++ experiments/summit/simple_mdff_vds_8_replicas_5angstrom_20iter_5k_numsteps/make_readable_dir.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-iter_cnt=0
+declare -A iter_cnt
 base_path="readable"
 for unit in `ls -d unit*`
 do
 	rp_unit_name=`grep RP_UNIT_NAME $unit/unit*.sh|cut -d= -f2|tr -d '"'`
 	pipe_name=`echo "$rp_unit_name"|cut -d, -f6`
+	if [ ! ${iter_cnt[$pipe_name]} ]
+	then
+		iter_cnt[$pipe_name]=0
+	fi
+	echo $pipe_name, iter_cnt:${iter_cnt[$pipe_name]}
+
 	stage_name=`echo "$rp_unit_name"|cut -d, -f4|tr ' ' '_'`
 	mkdir -p $base_path/$pipe_name
-	new_dir="$base_path/$pipe_name/$iter_cnt.$stage_name"
+	echo mkdir -p $base_path/$pipe_name
+	new_dir="$base_path/$pipe_name/${iter_cnt[$pipe_name]}.$stage_name"
 	if [ -d "$new_dir" ]
 	then
-		iter_cnt=`expr $iter_cnt + 1`
+		iter_cnt[$pipe_name]=`expr ${iter_cnt[$pipe_name]} + 1`
+		echo $new_dir exist
+		echo $iter_cnt
 	fi
-	new_dir="$base_path/$pipe_name/$iter_cnt.$stage_name"
+	new_dir="$base_path/$pipe_name/${iter_cnt[$pipe_name]}.$stage_name"
 
 	cp -pr $unit $new_dir
 	echo cp -pr $unit $new_dir
